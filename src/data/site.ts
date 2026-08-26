32be6259e19970123e557b4c8026c9a3963efdb6
sherif abdulazeez <sherifabdulazeez1810@gmail.com>
2026-08-26 14:27:23 +0100

    site update

--- src/data/site.ts
+++ src/data/site.ts
@@ -2,6 +2,6 @@ export const site = {
   name: "Sherif Abdulazeez",
   role: "Frontend Developer",
   github: "https://github.com/sherifCodess",
-  linkedin: "linkedin.com/in/sherif-abdulazeez-982bb4396",
-  email: "abdulazeezsherif1810@gmail.com",
+  linkedin: "https://www.linkedin.com/in/sherif-abdulazeez-982bb4396",
+  email: "mailti:abdulazeezsherif1810@gmail.com",
 };
